9620d4baaac4fb6dbd68d353e65e36a64b696dfd
Daan Ketelaars <daanketelaars@gmail.com>
2020-11-18 12:32:26 +0100

    update 181120

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,57 @@
-// JavaScript Document
+/*********************
+
+CONTENT: 
+01 - HAMBURGER MENU
+02 - BACKGROUND IMAGES (HOMEPAGE)
+
+***********/
+
+
+
+
+
+/* HAMBURGER MENU */
+const hamMenu = document.querySelector('.btn_menu');
+const subNav = document.querySelector('.sub_nav');
+const overlayMenu = document.querySelector('.overlay');
+
+
+hamMenu.addEventListener('click', function () {
+    hamMenu.classList.toggle('open_hamMenu');
+    subNav.classList.toggle('open_subNav');
+    overlayMenu.classList.toggle('open_overlay');
+});
+
+
+
+/* BACKGROUND IMAGES (HOMEPAGE) */
+
+const bgImg = document.querySelectorAll('[data-bg');
+const screenWidth = window.innerWidth;
+
+for (let index = 0; index < bgImg.length; index++) {
+
+    if (screenWidth < 768) {
+
+        bgImg[index].style.backgroundImage = 'url(' + bgImg[index].getAttribute('data-bg-mobile') + ')';
+
+    } else {
+        bgImg[index].style.backgroundImage = 'url(' + bgImg[index].getAttribute('data-bg-desktop') + ')';
+    }
+}
+
+// var imageBgs = document.querySelectorAll('[data-bg]');
+// var screenWidth = window.innerWidth;
+
+// for(var i=0; i<imageBgs.length; i++) {
+//     if( screenWidth < 768 ){
+//         // Load mobile image
+//         imageBgs[i].style.backgroundImage = 'url('+imageBgs[i].getAttribute('data-bg-img-mobile')+')';
+//     } else if( screenWidth >= 768 && screenWidth <= 1024 ) {
+//         // Ipad
+//         imageBgs[i].style.backgroundImage = 'url('+imageBgs[i].getAttribute('data-bg-img-tablet')+')';
+//     } else {
+//         // desktop image
+//         imageBgs[i].style.backgroundImage = 'url('+imageBgs[i].getAttribute('data-bg-img-desktop')+')';
+//     }
+// }
