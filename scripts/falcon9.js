840ba12ee70d429b296684bc4ac1af7f4d23997c
Daan Ketelaars <daanketelaars@gmail.com>
2021-01-19 18:52:45 +0100

    update 19/01/2021

--- scripts/falcon9.js
+++ scripts/falcon9.js
@@ -6,23 +6,20 @@ CONTENT:
 ***********/
 /* VIDEO MODAL */
 
-
-
 /* collect all queryselectors */
-const videoModal = document.querySelector('.video-modal');
-const videoPlayBtn = document.querySelector('.play-btn');
-const videoSection = document.querySelector('.video_section');
-const changeHeader = document.querySelector('header');
-const closeModalBtn = document.querySelector('.close-modal');
+const videoModal = document.querySelector(".video-modal");
+const videoPlayBtn = document.querySelector(".play-btn");
+const videoSection = document.querySelector(".video_section");
+const changeHeader = document.querySelector("header");
+const closeModalBtn = document.querySelector(".close-modal");
 
 /* simple addeventlistener with function to activate style and classlist properties */
-videoPlayBtn.addEventListener('click', () => {
+videoPlayBtn.addEventListener("click", () => {
+    videoModal.classList.add("slideinup");
+    videoModal.classList.remove("slideoutdown");
 
-    videoModal.classList.add('slideinup');
-    videoModal.classList.remove('slideoutdown');
-
-    videoSection.classList.add('slideawayup');
-    videoSection.classList.remove('slideawaydown');
+    videoSection.classList.add("slideawayup");
+    videoSection.classList.remove("slideawaydown");
 
     videoModal.style.zIndex = "9999";
     videoModal.style.visibility = "visible";
@@ -34,15 +31,58 @@ videoPlayBtn.addEventListener('click', () => {
 });
 
 /* simple addeventlistener with function to activate style and classlist properties */
-closeModalBtn.addEventListener('click', () => {
+closeModalBtn.addEventListener("click", () => {
+    videoModal.classList.add("slideoutdown");
+    videoModal.classList.remove("slideinup");
 
-    videoModal.classList.add('slideoutdown');
-    videoModal.classList.remove('slideinup');
-
-    videoSection.classList.remove('slideawayup');
-    videoSection.classList.add('slideawaydown');
+    videoSection.classList.remove("slideawayup");
+    videoSection.classList.add("slideawaydown");
 
     changeHeader.style.zIndex = "";
 
     closeModalBtn.style.display = "none";
+});
+
+/* GSAP IMAGE CHANGE ON SCROLL */
+
+
+/* Register GSAP ScrollTrigger Plugin */
+gsap.registerPlugin(ScrollTrigger);
+
+
+/* function to add the right z-index to the bg images */
+// more info: https://greensock.com/forums/topic/25163-understanding-gsapset-method/ 
+// or: https://greensock.com/forums/topic/26670-create-pinned-parallax-with-fading-background-images/
+gsap.set(".bg", {
+    zIndex: (i, target, targets) => targets.length - i
+});
+
+/* Pauses animation */
+// https://greensock.com/docs/v3/GSAP/Timeline/paused()
+const imageChange = gsap.timeline({
+    paused: false
+});
+
+/* set duration and other animation effects to fade in and out */
+imageChange
+    .to("body", {
+        duration: 5.0
+    }, 0)
+    .to(".bg.one", {
+        duration: 3.0,
+        autoAlpha: 0
+    }, 3.0)
+    .to(".bg.two", {
+        duration: 3.0,
+        autoAlpha: 1
+    }, 3.0);
+
+/* create the scrolltrigger, pin the items to stay fixed and add the start + end point of the animation. */
+ScrollTrigger.create({
+    trigger: ".rocket_change_section",
+    start: "top top",
+    end: "+=200%",
+    pin: ".bg-wrap",
+    animation: imageChange,
+    scrub: true,
 });
